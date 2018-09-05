389e40a78f5cb5738765d79338ac5c9b566f6307
Herohtar <belac1186@gmail.com>
2018-09-04 22:17:04 -0500

    Updated cloud functions to modern JS

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,6 +1,8 @@
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import * as nodemailer from 'nodemailer';
+import express from 'express';
+import cors from 'cors';
 
 // Configure firebase-admin
 const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG);
@@ -23,91 +25,82 @@ const mailTransport = nodemailer.createTransport({
   }
 });
 
-exports.initializeUserData = functions.auth.user().onCreate(user => {
+exports.initializeUserData = functions.auth.user().onCreate(async user => {
   // Not checking if email is verified since only Google sign-in is enabled
-  const tokenRef = firestore.collection('userTokens').doc(user.uid);
-  const userRef = firestore.collection('users').doc(user.uid);
-  const permissionsRef = firestore.collection('userPermissions').doc(user.email);
+  await firestore.collection('userTokens').doc(user.uid).set( { accountStatus: 'pending' })
 
-  return firestore.runTransaction(transaction =>
-    transaction.get(permissionsRef).then(permissionsDoc => {
-      transaction.set(tokenRef, { accountStatus: 'pending' });
-      transaction.set(userRef, { name: user.displayName, email: user.email });
+  const tokenRef = firestore.collection('userTokens').doc(user.uid)
+  const userRef = firestore.collection('users').doc(user.uid)
+  const permissionsRef = firestore.collection('userPermissions').doc(user.email)
 
-      const permissions = permissionsDoc.data();
-      const customClaims = {
-        admin: permissions ? permissions.admin : false,
-        hasAccess: permissions ? permissions.hasAccess : false,
-      };
+  await firestore.runTransaction(async transaction => {
+    const permissionsDoc = await transaction.get(permissionsRef)
+    const permissions = permissionsDoc.data();
+    const customClaims = {
+      admin: permissions ? permissions.admin : false,
+      hasAccess: permissions ? permissions.hasAccess : false,
+    };
 
-      transaction.update(userRef, { getEmails: customClaims.hasAccess });
-
-      return admin.auth().setCustomUserClaims(user.uid, customClaims).then(() => transaction.update(tokenRef, { accountStatus: 'ready', refreshTime: admin.firestore.FieldValue.serverTimestamp() }));
-    })
-  );
+    transaction.set(userRef, { name: user.displayName, email: user.email, getEmails: customClaims.hasAccess })
+    await admin.auth().setCustomUserClaims(user.uid, customClaims)
+    transaction.update(tokenRef, { accountStatus: 'ready', refreshTime: admin.firestore.FieldValue.serverTimestamp() })
+  });
 })
 
-exports.removeUserData = functions.auth.user().onDelete(user => {
-  return Promise.all([
-    firestore.collection('users').doc(user.uid).delete(),
-    firestore.collection('userTokens').doc(user.uid).delete(),
-  ])
+exports.removeUserData = functions.auth.user().onDelete(async user => {
+  await firestore.collection('users').doc(user.uid).delete()
+  await firestore.collection('userTokens').doc(user.uid).delete()
 })
 
 // Sends a notification email when a new order is created
-exports.sendOrderEmail = functions.firestore.document('orders/{orderId}').onCreate((snapshot, context) => {
-  const data = snapshot.data();
-  return sendOrderEmail(data);
+exports.sendOrderEmail = functions.firestore.document('orders/{orderId}').onCreate(async (snapshot, context) => {
+  const data = snapshot.data()
+  await sendOrderEmail(data)
 })
 
-function getEmailsToNotify() {
-  return firestore.collection('users').where('getEmails', '==', true).get().then(results => results.docs.map(user => `${user.get('name')} <${user.get('email')}>`).join(','));
+async function getEmailsToNotify() {
+  const results = await firestore.collection('users').where('getEmails', '==', true).get();
+  return results.docs.map(user => `${user.get('name')} <${user.get('email')}>`).join(',');
 }
 
 // Sends an order email
-function sendOrderEmail({name, email, delivery, address, products}) {
-  return getEmailsToNotify().then(emails => {
-    const mailOptions = {
-      from: `Grammy\'s Gluten Free Goodies <${functions.config().email.noreply}>`,
-      to: `Undisclosed Recipients <${functions.config().email.noreply}>`,
-      bcc: emails,
-      subject: `New order from ${name}!`,
-    }
+async function sendOrderEmail({name, email, delivery, address, products}) {
+  const emails = await getEmailsToNotify();
 
-    var emailText = `Name: ${name}
-  Email: ${email}
-  Delivery: ${delivery}`
+  const mailOptions = {
+    from: `Grammy\'s Gluten Free Goodies <${functions.config().email.noreply}>`,
+    to: `Undisclosed Recipients <${functions.config().email.noreply}>`,
+    bcc: emails,
+    subject: `New order from ${name}!`,
+  }
 
-    if (delivery === 'yes') {
-      emailText = emailText.concat('\n', 'Address: ', address);
-    }
-    products.forEach(product => emailText = emailText.concat('\n', product.name, ': ', product.amount))
-    mailOptions.text = emailText
+  var emailText = `Name: ${name}
+Email: ${email}
+Delivery: ${delivery}`
 
-    var emailHtml = `<b>Name:</b> ${name}<br/>
-  <b>Email:</b> ${email}<br/>
-  <b>Delivery:</b> ${delivery}`
+  if (delivery === 'yes') {
+    emailText = emailText.concat('\n', 'Address: ', address);
+  }
+  products.forEach(product => emailText = emailText.concat('\n', product.name, ': ', product.amount))
+  mailOptions.text = emailText
 
-    if (delivery === 'yes') {
-      emailHtml = emailHtml.concat('<br/><b>', 'Address:</b> ', address);
-    }
-    products.forEach(product => emailHtml = emailHtml.concat('<br/><b>', product.name, ':</b> ', product.amount))
-    mailOptions.html = emailHtml
+  var emailHtml = `<b>Name:</b> ${name}<br/>
+<b>Email:</b> ${email}<br/>
+<b>Delivery:</b> ${delivery}`
 
-    return mailTransport.sendMail(mailOptions).then(() => {
-      return console.log('New order email sent.');
-    })
-  })
+  if (delivery === 'yes') {
+    emailHtml = emailHtml.concat('<br/><b>', 'Address:</b> ', address);
+  }
+  products.forEach(product => emailHtml = emailHtml.concat('<br/><b>', product.name, ':</b> ', product.amount))
+  mailOptions.html = emailHtml
+
+  await mailTransport.sendMail(mailOptions)
+  console.log('New order email sent.')
 }
 
-const express = require('express')
-const cors = require('cors')
-
 const orderApp = express()
-
 orderApp.use(cors())
-
-orderApp.post('/create', (req, res) => {
+orderApp.post('/create', async (req, res) => {
   const data = req.body
   var order = {
     name: data.name,
@@ -121,7 +114,8 @@ orderApp.post('/create', (req, res) => {
   if (order.delivery === 'yes') {
     order.address = data.address
   }
-  firestore.collection('orders').add(order).then(() => res.json({type: 'success', message: 'Order created successfully.'}))
+  await firestore.collection('orders').add(order)
+  res.json({type: 'success', message: 'Order created successfully.'})
 })
 
 exports.orders = functions.https.onRequest(orderApp)
