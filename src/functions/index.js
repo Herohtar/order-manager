ee48c7848b478adb563bcac997c94818ed5024a4
Herohtar <belac1186@gmail.com>
2018-09-08 23:48:38 -0500

    Added confirmation emails to cloud functions

--- src/functions/index.js
+++ src/functions/index.js
@@ -25,6 +25,8 @@ const mailTransport = nodemailer.createTransport({
   }
 });
 
+const siteName = 'Grammy\'s Gluten Free Goodies';
+
 exports.initializeUserData = functions.auth.user().onCreate(async user => {
   // Not checking if email is verified since only Google sign-in is enabled
   await firestore.collection('userTokens').doc(user.uid).set( { accountStatus: 'pending' })
@@ -52,10 +54,11 @@ exports.removeUserData = functions.auth.user().onDelete(async user => {
   await firestore.collection('userTokens').doc(user.uid).delete()
 })
 
-// Sends a notification email when a new order is created
-exports.sendOrderEmail = functions.firestore.document('orders/{orderId}').onCreate(async (snapshot, context) => {
+// Sends emails when a new order is created
+exports.sendEmails = functions.firestore.document('orders/{orderId}').onCreate(async (snapshot, context) => {
   const data = snapshot.data()
   await sendOrderEmail(data)
+  await sendConfirmationEmail(data)
 })
 
 async function getEmailsToNotify() {
@@ -63,39 +66,63 @@ async function getEmailsToNotify() {
   return results.docs.map(user => `${user.get('name')} <${user.get('email')}>`).join(',');
 }
 
-// Sends an order email
+function getOrderDetailString(type, name, email, delivery, address, products) {
+  var detailString;
+
+  if (type.toLowerCase() === 'html') {
+    detailString = `<b>Name:</b> ${name}<br/>
+<b>Email:</b> ${email}<br/>
+<b>Delivery:</b> ${delivery}`
+
+    if (delivery === 'yes') {
+      detailString = detailString.concat('<br/><b>', 'Address:</b> ', address);
+    }
+    products.forEach(product => detailString = detailString.concat('<br/><b>', product.name, ':</b> ', product.amount))
+  } else {
+    detailString = `Name: ${name}
+Email: ${email}
+Delivery: ${delivery}`
+
+    if (delivery === 'yes') {
+      detailString = detailString.concat('\n', 'Address: ', address);
+    }
+    products.forEach(product => detailString = detailString.concat('\n', product.name, ': ', product.amount))
+  }
+
+  return detailString;
+}
+
+// Sends an order notification email
 async function sendOrderEmail({name, email, delivery, address, products}) {
   const emails = await getEmailsToNotify();
 
   const mailOptions = {
-    from: `Grammy\'s Gluten Free Goodies <${functions.config().email.noreply}>`,
+    from: `${siteName} <${functions.config().email.noreply}>`,
     to: `Undisclosed Recipients <${functions.config().email.noreply}>`,
     bcc: emails,
     subject: `New order from ${name}!`,
   }
 
-  var emailText = `Name: ${name}
-Email: ${email}
-Delivery: ${delivery}`
-
-  if (delivery === 'yes') {
-    emailText = emailText.concat('\n', 'Address: ', address);
-  }
-  products.forEach(product => emailText = emailText.concat('\n', product.name, ': ', product.amount))
-  mailOptions.text = emailText
-
-  var emailHtml = `<b>Name:</b> ${name}<br/>
-<b>Email:</b> ${email}<br/>
-<b>Delivery:</b> ${delivery}`
-
-  if (delivery === 'yes') {
-    emailHtml = emailHtml.concat('<br/><b>', 'Address:</b> ', address);
-  }
-  products.forEach(product => emailHtml = emailHtml.concat('<br/><b>', product.name, ':</b> ', product.amount))
-  mailOptions.html = emailHtml
+  mailOptions.text = getOrderDetailString('text', name, email, delivery, address, products);
+  mailOptions.html = getOrderDetailString('html', name, email, delivery, address, products);
 
   await mailTransport.sendMail(mailOptions)
   console.log('New order email sent.')
+}
+
+// Sends a confirmation email
+async function sendConfirmationEmail({name, email, delivery, address, products}) {
+  const mailOptions = {
+    from: `${siteName} <${functions.config().email.noreply}>`,
+    to: `${name} <${email}>`,
+    subject: `Order Confirmation`,
+  }
+
+  mailOptions.text = `Thank you for your order from ${siteName}! Here are your order details:\n\n${getOrderDetailString('text', name, email, delivery, address, products)}`;
+  mailOptions.html = `Thank you for your order from <b>${siteName}</b>! Here are your order details:<br/><br/>${getOrderDetailString('html', name, email, delivery, address, products)}`;
+
+  await mailTransport.sendMail(mailOptions)
+  console.log('Confirmation email sent.')
 }
 
 const orderApp = express()
