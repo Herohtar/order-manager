5c0c09947d3ebe1887ab8ff8c428a6860e864655
Herohtar <belac1186@gmail.com>
2018-12-23 02:52:19 -0600

    Updated to use Location instead of Router hack

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Link } from '@reach/router'
+import { Location, Link } from '@reach/router'
 //
 import AuthDataContext from '../session/AuthDataContext'
 import Tabs from '@material-ui/core/Tabs'
@@ -9,26 +9,24 @@ import * as routes from '../constants/routes'
 
 const getAvailableRoutes = authData => Object.values(routes).filter(route => route.condition(authData))
 
-const Nav = ({ authData, location }) => {
-  const availableRoutes = getAvailableRoutes(authData)
-  const routeIndex = availableRoutes.findIndex(route => route.path === location.pathname)
-
-  return (
-    <Tabs value={(routeIndex >= 0) ? routeIndex : false} component="nav">
-      {availableRoutes.map(({ path, label }, index) => (
-        <Tab key={index} component={Link} value={index} to={path} label={label} />
-      ))}
-    </Tabs>
-  )
-}
-
 const Navigation = () => (
   <AuthDataContext.Consumer>
     {
       authData => (
-        <Router>
-          <Nav path="*" authData={authData} />
-        </Router>
+        <Location>
+          {({ location }) => {
+            const availableRoutes = getAvailableRoutes(authData)
+            const routeIndex = availableRoutes.findIndex(route => route.path === location.pathname)
+
+            return (
+              <Tabs value={(routeIndex >= 0) ? routeIndex : false} component="nav">
+                {availableRoutes.map(({ path, label }, index) => (
+                  <Tab key={index} component={Link} value={index} to={path} label={label} />
+                ))}
+              </Tabs>
+            )
+          }}
+        </Location>
       )
     }
   </AuthDataContext.Consumer>
