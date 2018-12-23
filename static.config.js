fd16f0e2f562b988b8d7477e867dbcddc62571aa
Herohtar <belac1186@gmail.com>
2018-12-23 02:13:32 -0600

    Moved page components to new page structure and removed manual route specification

--- static.config.js
+++ static.config.js
@@ -1,35 +1,12 @@
 import React from 'react'
 //
-import * as routes from './src/constants/routes'
-
 import siteConfig from './src/content/SiteConfig.json'
 
 export default {
   siteRoot: siteConfig.url,
   getSiteData: () => siteConfig,
   getRoutes: async () => {
-    return [
-      {
-        path: routes.Home.path,
-        component: routes.Home.component,
-      },
-      {
-        path: routes.Dashboard.path,
-        component: routes.Dashboard.component,
-      },
-      {
-        path: routes.Admin.path,
-        component: routes.Admin.component,
-      },
-      {
-        path: routes.Account.path,
-        component: routes.Account.component,
-      },
-      {
-        is404: true,
-        component: 'src/containers/404',
-      },
-    ]
+    return []
   },
   Document: ({ Html, Head, Body, children }) => (
     <Html>
