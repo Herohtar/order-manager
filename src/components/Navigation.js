2b33923708460656a3e63ce354ca0df97d3627e1
Herohtar <belac1186@gmail.com>
2019-01-09 22:23:17 -0600

    Clean the trailing slash from the location path

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,6 +7,11 @@ import Tab from '@material-ui/core/Tab'
 
 import * as routes from '../constants/routes'
 
+// location.pathname can be either '/some/path' or '/some/path/', but both are actually
+// the same route, so to make location matching work for both cases we need to remove the
+// trailing slash from the path string (with the exception of the root path, '/')
+const cleanPath = path => path.replace(/\/$/, '') || '/'
+
 const getAvailableRoutes = authData => Object.values(routes).filter(route => route.condition(authData))
 
 const Navigation = () => (
@@ -16,7 +21,7 @@ const Navigation = () => (
         <Location>
           {({ location }) => {
             const availableRoutes = getAvailableRoutes(authData)
-            const routeIndex = availableRoutes.findIndex(route => route.path === location.pathname)
+            const routeIndex = availableRoutes.findIndex(route => route.path === cleanPath(location.pathname))
 
             return (
               <Tabs value={(routeIndex >= 0) ? routeIndex : false} component="nav">
