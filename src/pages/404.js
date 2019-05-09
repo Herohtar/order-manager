e33752bc117cbc49d99b34e330a4624a7fda3fae
Herohtar <belac1186@gmail.com>
2019-05-08 20:13:18 -0500

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,28 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Head, useSiteData } from 'react-static'
 //
+import Typography from '@material-ui/core/Typography'
 
-export default class extends React.Component {
-  state = {
-    ready: false,
-  }
+export default () => {
+  const [ready, setReady] = useState(false)
 
-  componentDidMount() {
-    this.makeReady()
-  }
+  useEffect(() => {
+    setReady(true)
+  }, [])
 
-  makeReady = () => {
-    if (!this.state.ready) {
-      this.setState(() => ({
-        ready: true,
-      }))
-    }
-  }
+  const { title } = useSiteData()
 
-  render() {
-    return this.state.ready ? (
-      <div>
-        <h1>404 - Oh no's! We couldn't find that page :(</h1>
-      </div>
-    ) : null
-  }
+  return ready ? (
+    <div>
+      <Head title={`404 - Page Not Found - ${title}`} />
+      <Typography variant="h3" align="center" paragraph>404</Typography>
+      <Typography variant="h4" align="center">The page you requested could not be found.</Typography>
+    </div>
+  ) : null
 }
