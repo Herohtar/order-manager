0dd3697d029d1aa15c88378bb58fb9a563905bc5
Herohtar <belac1186@gmail.com>
2019-07-13 15:37:44 -0500

    Change fallback string

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
           <Navigation />
         </AppBar>
         <main>
-          <React.Suspense fallback="Loading...">
+          <React.Suspense fallback={'Loading...'}>
             <Routes />
           </React.Suspense>
         </main>
