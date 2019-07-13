9bba0e846aa7beb5b6b288e73230052b8fb01e1c
Herohtar <belac1186@gmail.com>
2019-07-13 15:35:31 -0500

    Update App.js to use new styles hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from 'react'
 import { Root, Routes } from 'react-static'
-import { compose } from 'recompose'
 //
 import withAuthentication from './session/withAuthentication'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/styles'
 import Navigation from './components/Navigation'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import AppBar from '@material-ui/core/AppBar'
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
   },
-})
+}))
 
-const App = ({ classes }) => (
-  <Root>
-    <div>
-      <CssBaseline />
-      <AppBar position="sticky" className={classes.appBar}>
-        <Navigation />
-      </AppBar>
-      <main>
-        <Routes />
-      </main>
-    </div>
-  </Root>
-)
+const App = () => {
+  const classes = useStyles()
 
-const enhance = compose(
-  withAuthentication,
-  withStyles(styles),
-)
+  return (
+    <Root>
+      <div>
+        <CssBaseline />
+        <AppBar position="sticky" className={classes.appBar}>
+          <Navigation />
+        </AppBar>
+        <main>
+          <Routes />
+        </main>
+      </div>
+    </Root>
+  )
+}
 
-export default enhance(App)
+export default withAuthentication(App)
