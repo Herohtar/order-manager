f2447d5e896ce35bf5b0c308dce4f732d84aea9b
Herohtar <belac1186@gmail.com>
2019-07-14 14:40:26 -0500

    Update Account to use hooks and MUI4

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { SiteData, Head } from 'react-static'
+import { Head, useSiteData } from 'react-static'
 //
 import { auth, firestore } from '../firebase'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/styles'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
@@ -12,16 +12,15 @@ import CardActions from '@material-ui/core/CardActions'
 import Button from '@material-ui/core/Button'
 import Snackbar from '@material-ui/core/Snackbar'
 import ErrorTwoToneIcon from '@material-ui/icons/ErrorTwoTone'
-import { compose } from 'recompose'
 import YesNoDialog from '../components/YesNoDialog'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Checkbox from '@material-ui/core/Checkbox'
 
 import withAuthorization from '../session/withAuthorization'
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing(3),
   },
   card: {
     display: 'flex',
@@ -40,98 +39,82 @@ const styles = theme => ({
     alignItems: 'stretch',
     justifyContent: 'space-evenly',
   },
-})
+}))
 
-class Account extends React.Component {
-  state = {
-    dialogOpen: false,
-    error: false,
-    errorMessage: null,
-    getEmails: false,
-  }
+const Account = (props) => {
+  const { authData } = props
+  const classes = useStyles()
+  const { title } = useSiteData()
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [error, setError] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [getEmails, setGetEmails] = useState(false)
 
-  componentDidMount() {
-    const { authData } = this.props
-    this.unsubscribeUserChanged = firestore.collection('users').doc(authData.authUser.uid).onSnapshot(doc => {
-      const getEmails = doc.get('getEmails')
-      this.setState(() => ({ getEmails }))
+  useEffect(() => {
+    const unsubscribeUserChanged = firestore.collection('users').doc(authData.authUser.uid).onSnapshot(doc => {
+      setGetEmails(doc.get('getEmails'))
     })
-  }
 
-  componentWillUnmount() {
-    this.unsubscribeUserChanged()
-  }
+    return () => unsubscribeUserChanged()
+  })
 
-  handleClose = (event, reason) => {
+  const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return
     }
 
-    this.setState({ error: false })
+    setError(false)
   }
 
-  handleDeleteClick = () => {
-    this.setState(() => ({ dialogOpen: true }))
+  const handleDeleteClick = () => {
+    setDialogOpen(true)
   }
 
-  handleNo = () => {
-    this.setState(() => ({ dialogOpen: false }))
+  const handleNo = () => {
+    setDialogOpen(false)
   }
 
-  handleYes = () => {
-    auth.currentUser.delete().catch(error =>
-      this.setState(() => ({ error: true, errorMessage: error.message }))
-    )
-    this.setState(() => ({dialogOpen: false }))
+  const handleYes = () => {
+    auth.currentUser.delete().catch(error => {
+      setError(true)
+      setErrorMessage(error.message)
+    })
+    setDialogOpen(false)
   }
 
-  handleGetEmailsChange = async (event) => {
-    const { authData } = this.props
+  const handleGetEmailsChange = async (event) => {
     await firestore.collection('users').doc(authData.authUser.uid).update({ getEmails: event.target.checked })
   }
 
-  render() {
-    const { authData, classes } = this.props
-    const { dialogOpen, error, errorMessage, getEmails } = this.state
 
-    return (
-      <Grid container justify="center" className={classes.root}>
-        <SiteData>
-          {({title}) => (
-            <Head title={`Account - ${title}`} />
-          )}
-        </SiteData>
-        <Grid item xs={4}>
-          <Card className={classes.card}>
-            <CardMedia image={authData.authUser.photoURL} className={classes.media} />
-            <CardContent className={classes.content}>
-              {(authData.accountStatus === 'pending') && <Typography variant="headline" paragraph>Account setup pending.</Typography>}
-              <Typography variant="h5">{authData.authUser.displayName}</Typography>
-              <Typography variant="subtitle1" paragraph>{authData.authUser.email}</Typography>
-              {(!!authData.token && (authData.token.claims.hasAccess || authData.token.claims.admin)) ?
-                <FormControlLabel control={<Checkbox checked={getEmails} onChange={this.handleGetEmailsChange} value="getEmails" />} label="Get order notification emails" />
-                :
-                null
-              }
-            </CardContent>
-            <CardActions className={classes.actions}>
-              <Button variant="contained" color="primary" onClick={() => auth.signOut()}>Sign Out</Button>
-              <Button variant="outlined" onClick={this.handleDeleteClick}>Delete Account</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <YesNoDialog open={dialogOpen} title="Delete account?" message="Are you sure you want to delete your account? You will be signed out and will no longer have accesss to your order dashboard." noText="No" yesText="Yes, delete my account" onNo={this.handleNo} onYes={this.handleYes} />
-        <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} open={error} onClose={this.handleClose} autoHideDuration={5000} message={errorMessage} action={<ErrorTwoToneIcon color="error" />} />
+  return (
+    <Grid container justify="center" className={classes.root}>
+      <Head title={`Account - ${title}`} />
+      <Grid item xs={4}>
+        <Card className={classes.card}>
+          <CardMedia image={authData.authUser.photoURL} className={classes.media} />
+          <CardContent className={classes.content}>
+            {(authData.accountStatus === 'pending') && <Typography variant="headline" paragraph>Account setup pending.</Typography>}
+            <Typography variant="h5">{authData.authUser.displayName}</Typography>
+            <Typography variant="subtitle1" paragraph>{authData.authUser.email}</Typography>
+            {(!!authData.token && (authData.token.claims.hasAccess || authData.token.claims.admin)) ?
+              <FormControlLabel control={<Checkbox checked={getEmails} onChange={handleGetEmailsChange} value="getEmails" />} label="Get order notification emails" />
+              :
+              null
+            }
+          </CardContent>
+          <CardActions className={classes.actions}>
+            <Button variant="contained" color="primary" onClick={() => auth.signOut()}>Sign Out</Button>
+            <Button variant="outlined" onClick={handleDeleteClick}>Delete Account</Button>
+          </CardActions>
+        </Card>
       </Grid>
-    )
-  }
+      <YesNoDialog open={dialogOpen} title="Delete account?" message="Are you sure you want to delete your account? You will be signed out and will no longer have accesss to your order dashboard." noText="No" yesText="Yes, delete my account" onNo={handleNo} onYes={handleYes} />
+      <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} open={error} onClose={handleClose} autoHideDuration={5000} message={errorMessage} action={<ErrorTwoToneIcon color="error" />} />
+    </Grid>
+  )
 }
 
 const authCondition = authUser => !!authUser
 
-const enhance = compose(
-  withAuthorization(authCondition),
-  withStyles(styles),
-)
-
-export default enhance(Account)
+export default withAuthorization(authCondition)(Account)
